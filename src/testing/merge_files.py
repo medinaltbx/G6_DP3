11238fc87f5f0a347d80639107f2fe29824e4620
medinazorin_desk <medinazorin@gmail.com>
2022-04-26 20:09:08 +0200

    Added merge files

--- src/testing/merge_files.py
+++ src/testing/merge_files.py
@@ -1,22 +1,25 @@
 import pandas as pd
 import os
 from functools import partial, reduce
-
+import datetime
+from dateutil.relativedelta import relativedelta
 pd.set_option('display.width',None)
+rdelta = relativedelta()
 
-set = 'train'
-path = rf"C:\Users\Cristian\Documents\repos\G6_DP3\data\raw_data\{set}"
+demograficos = pd.read_csv(r"C:\Users\Cristian\Documents\repos\G6_DP3\data\raw_data\train\train_datos_demograficos.csv")
+performance = pd.read_csv(r"C:\Users\Cristian\Documents\repos\G6_DP3\data\raw_data\train\train_performance.csv")
+demo_perf = demograficos.merge(performance, on="customerid", how="right")
+# print(demo_perf)
 
-dfs = []
-for root,dirs,files in os.walk(path):
-    for file in files:
-        print(file)
-        if file.endswith(".csv"):
-            df = pd.read_csv(path+"/"+file)
-            print(df)
-            dfs.append(df)
+previous = pd.read_csv(r"C:\Users\Cristian\Documents\repos\G6_DP3\data\raw_data\train\train_previous_loan.csv",parse_dates=['firstduedate','firstrepaiddate'])
+previous = previous.add_suffix("_prev")
+# previous['Difference'] = (previous['firstrepaiddate_prev'] - previous['firstduedate_prev']).astype('timedelta64[h]')
+previous['minutes_late'] = (previous.firstrepaiddate_prev - previous.firstduedate_prev) / pd.Timedelta(minutes=1)
 
 
-df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['customerid'],
-                                            how='right'), dfs)
-print(df_merged)
+print(previous)
+
+# merged = demo_perf.merge(previous, left_on="customerid", right_on="customerid_prev", how="left")
+# print(merged)
+
+
