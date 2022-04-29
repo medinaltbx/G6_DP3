f3df3a46db35a1cd748f86857d6044aed6f4de62
Cristian <medinazorin@gmail.com>
2022-04-29 16:08:53 +0200

    Added merged_train

--- src/testing/merge_files.py
+++ src/testing/merge_files.py
@@ -5,12 +5,12 @@ from preprocess_demograficos import manage_demograficos
 pd.set_option('display.width',None)
 
 
-performance = pd.read_csv(r"C:\Users\Cristian\Documents\repos\G6_DP3\data\raw_data\train\train_performance.csv")
+performance = pd.read_csv(r"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/test/test_performance.csv")
 performance = manage_current(performance)
 print('PERFORMANCE;')
 print(performance)
 
-demograficos = pd.read_csv(r"C:\Users\Cristian\Documents\repos\G6_DP3\data\raw_data\train\train_datos_demograficos.csv",
+demograficos = pd.read_csv(r"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/test/test_datos_demograficos.csv",
                             parse_dates=['birthdate'])
 demograficos = manage_demograficos(demograficos)
 print('DEMOGRAFICOS')
@@ -20,7 +20,7 @@ demo_perf = demograficos.merge(performance, on="customerid", how="inner")
 print('DEMO_PERF')
 print(demo_perf)
 
-previous = pd.read_csv(r"C:\Users\Cristian\Documents\repos\G6_DP3\data\raw_data\train\train_previous_loan.csv",parse_dates=['firstduedate','firstrepaiddate'])
+previous = pd.read_csv(r"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/test/test_previous_loan.csv",parse_dates=['firstduedate','firstrepaiddate'])
 previous = manage_previous(previous)
 print('PREVIOUS')
 print(previous)
@@ -29,6 +29,6 @@ merged = demo_perf.merge(previous, on="customerid", how="left")
 print('MERGED')
 print(merged)
 
-merged.to_csv(r"C:\Users\Cristian\Documents\repos\G6_DP3\data\merged_data\train\merged_train.csv",sep=';',index=False)
+merged.to_csv(r"C:\Users\Cristian Medina\Documents\EDEM\G6_DP3\data\merged_data\test\merged_test.csv",sep=';',index=False)
 
 
