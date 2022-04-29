39a4e1fd6284d6e82f110787b96e90142a61c09c
Cristian <medinazorin@gmail.com>
2022-04-29 18:07:13 +0200

    Added baseline model pickle

--- src/model/baseline.py
+++ src/model/baseline.py
@@ -2,11 +2,13 @@ import pandas as pd
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import pickle
 pd.set_option("display.width",None)
 
 df = pd.read_csv(r"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/merged_data/train/merged_train.csv",sep=';')
 df.drop(["customerid","longitude_gps", "latitude_gps"],axis=1,inplace=True)
 str_cols = ["loanamount", "totaldue", "Commision"]
+# Remove decimals from string
 for c in str_cols:
     df[c] = df[c].str.split(',').str[0]
 df[str_cols] = df[str_cols].apply(pd.to_numeric)
@@ -21,4 +23,8 @@ baseline_model.fit(X_train, y_train)
 
 y_pred = baseline_model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
+
+with open(r"C:\Users\Cristian Medina\Documents\EDEM\G6_DP3\data\models\baseline.pkl","wb") as file:
+    pickle.dump(baseline_model, file)
+
 print("Accuracy: ", accuracy)
