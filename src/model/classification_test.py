f73b62ecc06b3d07c1a6b6b7080003d223173520
Cristian <medinazorin@gmail.com>
2022-04-29 17:45:30 +0200

    Added model script

--- src/model/classification_test.py
+++ src/model/classification_test.py
@@ -5,11 +5,11 @@ from sklearn.metrics import accuracy_score
 pd.set_option("display.width",None)
 
 df = pd.read_csv(r"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/merged_data/train/merged_train.csv",sep=';')
-df.drop(["customerid"],axis=1,inplace=True)
-str_cols = []
+df.drop(["customerid","longitude_gps", "latitude_gps"],axis=1,inplace=True)
+str_cols = ["loanamount", "totaldue", "Commision"]
+for c in str_cols:
+    df[c] = df[c].str.split(',').str[0]
 df[str_cols] = df[str_cols].apply(pd.to_numeric)
-pd.to_numeric(s)
-print(df)
 X, y = df.drop(["good_bad_flag"],axis=1), df['good_bad_flag']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
