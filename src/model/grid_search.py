a9d06cedd30552f9ae80cdb59099554379212abe
Cristian <medinazorin@gmail.com>
2022-04-29 19:00:52 +0200

    Added all numeric data

---
+++ src/model/grid_search.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from numpy.ma.bench import timer
+from sklearn.model_selection import train_test_split, StratifiedKFold, RandomizedSearchCV
+from sklearn.metrics import accuracy_score
+import xgboost as xgb
+from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
+import pickle
+
+pd.set_option("display.width",None)
+
+
+df = pd.read_csv(r"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/merged_data/train/merged_train.csv",sep=';')
+df.drop(["customerid","longitude_gps", "latitude_gps"],axis=1,inplace=True)
+str_cols = ["loanamount", "totaldue", "Commision"]
+# Remove decimals from string
+for c in str_cols:
+    df[c] = df[c].str.split(',').str[0]
+df[str_cols] = df[str_cols].apply(pd.to_numeric)
+X, y = df.drop(["good_bad_flag"],axis=1), df['good_bad_flag']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+
+space={'max_depth': hp.quniform("max_depth", 3, 18, 1),
+        'gamma': hp.uniform ('gamma', 1,9),
+        'reg_alpha' : hp.quniform('reg_alpha', 40,180,1),
+        'reg_lambda' : hp.uniform('reg_lambda', 0,1),
+        'colsample_bytree' : hp.uniform('colsample_bytree', 0.5,1),
+        'min_child_weight' : hp.quniform('min_child_weight', 0, 10, 1),
+        'n_estimators': 180,
+        'seed': 0
+    }
+
+
+def objective(space):
+    clf = xgb.XGBClassifier(
+        n_estimators=space['n_estimators'], max_depth=int(space['max_depth']), gamma=space['gamma'],
+        reg_alpha=int(space['reg_alpha']), min_child_weight=int(space['min_child_weight']),
+        colsample_bytree=int(space['colsample_bytree']))
+
+    evaluation = [(X_train, y_train), (X_test, y_test)]
+
+    clf.fit(X_train, y_train,
+            eval_set=evaluation, eval_metric="auc",
+            early_stopping_rounds=10, verbose=False)
+
+    pred = clf.predict(X_test)
+    accuracy = accuracy_score(y_test, pred > 0.5)
+    print("SCORE:", accuracy)
+    return {'loss': -accuracy, 'status': STATUS_OK}
+
+trials = Trials()
+
+best_hyperparams = fmin(fn = objective,
+                        space = space,
+                        algo = tpe.suggest,
+                        max_evals = 100,
+                        trials = trials)
+
+print("The best hyperparameters are : ","\n")
+print(best_hyperparams)
--- src/preprocess/merge_files.py
+++ src/preprocess/merge_files.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import preprocess
 
-performance = pd.read_csv(r"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/test/test_performance.csv")
+
+set = "test"
+
+performance = pd.read_csv(rf"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/{set}/{set}_performance.csv")
 performance = preprocess.manage_current(performance)
 # print('PERFORMANCE;')
 # print(performance)
 
-demograficos = pd.read_csv(r"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/test/test_datos_demograficos.csv",
+demograficos = pd.read_csv(rf"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/{set}/{set}_datos_demograficos.csv",
                             parse_dates=['birthdate'])
 demograficos = preprocess.manage_demograficos(demograficos)
 # print('DEMOGRAFICOS')
@@ -16,13 +19,22 @@ demo_perf = demograficos.merge(performance, on="customerid", how="inner")
 # print('DEMO_PERF')
 # print(demo_perf)
 
-previous = pd.read_csv(r"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/test/test_previous_loan.csv",parse_dates=['firstduedate','firstrepaiddate'])
+previous = pd.read_csv(rf"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/{set}/{set}_previous_loan.csv",parse_dates=['firstduedate','firstrepaiddate'])
 previous = preprocess.manage_previous(previous)
 # print('PREVIOUS')
 # print(previous)
 
 merged = demo_perf.merge(previous, on="customerid", how="left")
-# print('MERGED')
-# print(merged)
+print('MERGED')
+print(merged)
 
-merged.to_csv(r"C:\Users\Cristian Medina\Documents\EDEM\G6_DP3\data\merged_data\test\merged_test.csv",sep=';',index=False)
+merged.drop(["longitude_gps", "latitude_gps"],axis=1,inplace=True)
+str_cols = ["loanamount", "totaldue"]
+# print(merged[str_cols].dtypes)
+# # Remove decimals from string
+# for c in str_cols:
+#     print(merged[c])
+#     merged[c] = merged[c].str.split(',').str[0]
+merged[str_cols] = merged[str_cols].apply(pd.to_numeric)
+
+merged.to_csv(rf"C:\Users\Cristian Medina\Documents\EDEM\G6_DP3\data\merged_data\{set}\merged_{set}.csv",sep=';',index=False)
