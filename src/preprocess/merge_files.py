85e33a4f6456a1ceeabdf92118df3e825115f8e0
Cristian <medinazorin@gmail.com>
2022-04-29 19:16:41 +0200

    Change str to numeric

--- src/preprocess/merge_files.py
+++ src/preprocess/merge_files.py
@@ -2,39 +2,42 @@ import pandas as pd
 import preprocess
 
 
-set = "test"
+for set in ["train","test"]:
+    performance = pd.read_csv(rf"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/{set}/{set}_performance.csv",decimal=",")
+    performance = preprocess.manage_current(performance)
+    # print('PERFORMANCE;')
+    # print(performance)
 
-performance = pd.read_csv(rf"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/{set}/{set}_performance.csv")
-performance = preprocess.manage_current(performance)
-# print('PERFORMANCE;')
-# print(performance)
+    demograficos = pd.read_csv(rf"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/{set}/{set}_datos_demograficos.csv",
+                                parse_dates=['birthdate'],decimal=".")
+    demograficos = preprocess.manage_demograficos(demograficos)
+    # print('DEMOGRAFICOS')
+    # print(demograficos)
 
-demograficos = pd.read_csv(rf"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/{set}/{set}_datos_demograficos.csv",
-                            parse_dates=['birthdate'])
-demograficos = preprocess.manage_demograficos(demograficos)
-# print('DEMOGRAFICOS')
-# print(demograficos)
+    demo_perf = demograficos.merge(performance, on="customerid", how="inner")
+    # print('DEMO_PERF')
+    # print(demo_perf)
 
-demo_perf = demograficos.merge(performance, on="customerid", how="inner")
-# print('DEMO_PERF')
-# print(demo_perf)
+    previous = pd.read_csv(rf"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/{set}/{set}_previous_loan.csv",parse_dates=['firstduedate','firstrepaiddate'],decimal=".")
+    previous = preprocess.manage_previous(previous)
+    # print('PREVIOUS')
+    # print(previous)
 
-previous = pd.read_csv(rf"https://raw.githubusercontent.com/medinaltbx/G6_DP3/master/data/raw_data/{set}/{set}_previous_loan.csv",parse_dates=['firstduedate','firstrepaiddate'])
-previous = preprocess.manage_previous(previous)
-# print('PREVIOUS')
-# print(previous)
+    merged = demo_perf.merge(previous, on="customerid", how="left")
+    print('MERGED')
+    print(merged)
 
-merged = demo_perf.merge(previous, on="customerid", how="left")
-print('MERGED')
-print(merged)
+    merged.drop(["longitude_gps", "latitude_gps"],axis=1,inplace=True)
+    str_cols = ["loanamount", "totaldue"]
+    print(merged[str_cols].dtypes)
+    # Remove decimals from string
+    # for c in str_cols:
+    #     print(merged[c])
+    #     merged[c] = merged[c].str.split(',').str[0]
 
-merged.drop(["longitude_gps", "latitude_gps"],axis=1,inplace=True)
-str_cols = ["loanamount", "totaldue"]
-# print(merged[str_cols].dtypes)
-# # Remove decimals from string
-# for c in str_cols:
-#     print(merged[c])
-#     merged[c] = merged[c].str.split(',').str[0]
-merged[str_cols] = merged[str_cols].apply(pd.to_numeric)
+    merged[str_cols] = merged[str_cols].apply(pd.to_numeric)
+    print(merged.dtypes)
 
-merged.to_csv(rf"C:\Users\Cristian Medina\Documents\EDEM\G6_DP3\data\merged_data\{set}\merged_{set}.csv",sep=';',index=False)
+    print(merged)
+    merged.to_csv(rf"C:\Users\Cristian Medina\Documents\EDEM\G6_DP3\data\merged_data\{set}\merged_{set}.csv",sep=';',index=False)
+    exit(0)
