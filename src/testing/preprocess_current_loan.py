e2476c8fe9307c114d572517388365dd4d484dcb
Cristian <medinazorin@gmail.com>
2022-04-30 09:46:39 +0200

    Updated structure

--- src/testing/preprocess_current_loan.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-pd.set_option('display.width',None)
-
-def manage_current(current):
-    current.replace({'good_bad_flag': {'Good': 1, 'Bad': 0}},inplace=True)
-    current.drop(['systemloanid','approveddate','creationdate'],axis=1,inplace=True)
-    current['referredby'] = [1 if isinstance(s, str) else 0 for s in current['referredby']]
-    return current
-
